75636fb62ed72a1fc6ed9547df79689e378f87d9
alkmanistik <42465362+alkmanistik@users.noreply.github.com>
2024-04-15 13:58:41 +0500

    Base
    Начало проекта

---
+++ gdscript/Global.gd
@@ -0,0 +1,17 @@
+"""
+	Глобальный скрипт я думаю его ещё раззделить на несколько частей
+	тут должно быть сохранение, чтение курсов, запись курсов и т.д.
+	функции которые используются везде
+"""
+
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ gdscript/block/Theory.gd
@@ -0,0 +1,16 @@
+"""
+	Класс данных теории
+	Содержимое можно посмотреть в пдф
+"""
+
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
